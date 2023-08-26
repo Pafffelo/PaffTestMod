466975af0a463386ef77a2b3efa5668eb30274f5
Pafffelo <pawdio225@gmail.com>
2023-08-26 15:05:01 +0200

    new item stuff

--- src/main/java/net/pafffelo/testmod/TestMod.java
+++ src/main/java/net/pafffelo/testmod/TestMod.java
@@ -1,6 +1,7 @@
 package net.pafffelo.testmod;
 
 import net.fabricmc.api.ModInitializer;
+import net.pafffelo.testmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,6 +11,7 @@ public class TestMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Hello Fabric worldo!");
+		LOGGER.info("Initializing test mod shit!");
+		ModItems.registerModItems();
 	}
 }
