bb880f90ae470705e0193fe89ee11089579728b3
Pafffelo <pawdio225@gmail.com>
2023-08-26 13:05:58 +0200

    idk how to use this

---
+++ src/main/java/net/pafffelo/testmod/TestModClient.java
@@ -0,0 +1,10 @@
+package net.pafffelo.testmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TestModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
